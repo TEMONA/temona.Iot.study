dfa6488a24106ff6ff729693b6f0101b6ede79ca
Jerry Lee <lzjjerrywoodylee@gmail.com>
2016-07-05 11:27:26 +0900

    ゴミの掃除とサーバーのポート指定とREADME

--- RasPi_Study/201607/thermometer_app/hdc1000/dummy.js
+++
@@ -1,7 +0,0 @@
-process.on("message", function (msg) {
-  console.log(msg);
-  var dummy_data = { Temp: 24.7048950195, Humi: 39.2150878906 }
-  setTimeout(function() {
-    process.send(dummy_data);
-  }, 1000);
-});
